b87d351a92d15cda5d40d8a287a7e6abde3e1cb8
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-09-08 10:53:27 -0500

    create project

---
+++ android/app/src/main/kotlin/com/example/app_directorio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_directorio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/iu/colors/global_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// HomePrincipal color
+const Color kHomePrincipalColor = Color(0xFF101575);
+
+//Background color
+const Color kBackgroundColor = Color.fromARGB(255, 223, 234, 230);
+
+//Navigation color
+// ignore: constant_identifier_names
+const Color AppBarColor = Color(0xFF101575);
+
+// ignore: constant_identifier_names
+const Color SombraBotom = Color(0xFF101575);
