bed53f2beaabf03c0174e1a1a9b91dce6fc16d5c
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-09-08 11:24:26 -0500

    add pages

--- lib/iu/pages/pages_navbar/home_page.dart
+++ lib/iu/pages/pages_navbar/home_page.dart
@@ -1,8 +1,97 @@
+import 'package:flutter/material.dart';
 // import 'package:catalogo_species/iu/Widgets/circleAvatar/circleAvatar.dart';
 // import 'package:catalogo_species/iu/Widgets/cards/home_cards/home_card_comunidades.dart';
-// import 'package:flutter/material.dart';
-
 // import '../../Widgets/drawer/Drawer.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  // ignore: unused_field
+  int _counter = 0;
+  // ignore: unused_field
+  int _selectedIndex = 0;
+  // ignore: unused_field
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+    ),
+    Text(
+      'Index 1: Business',
+    ),
+    Text(
+      'Index 2: School',
+    ),
+  ];
+  // ignore: unused_field
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // ignore: unused_field
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  // ignore: unused_field
+  int _currentindex = 0;
+  // ignore: unused_field
+  late Widget _currentWidget = Homepage();
+  // ignore: unused_field
+  final List<Widget> _children = [
+    Homepage(),
+    // Especiepage(),
+    // Comunidpage(),
+  ];
+  // ignore: unused_field
+  void onTappedBar(int index) {
+    setState(() {
+      _currentindex = index;
+      _currentWidget = _children[index];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('Amazonia'),
+      // ),
+      // drawer: const drawer(),
+      body: _currentWidget,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+            backgroundColor: Colors.deepPurple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Especies',
+            backgroundColor: Colors.deepPurple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Comunidades',
+            backgroundColor: Colors.deepPurple,
+          ),
+        ],
+        currentIndex: _currentindex,
+        selectedItemColor: Colors.amber[800],
+        onTap: onTappedBar,
+      ),
+    );
+  }
+}
 
 // class Homepage extends StatefulWidget {
 //   const Homepage({super.key});
